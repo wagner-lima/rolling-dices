f7feaa79c8e083bf34324007c27789a970ac380a
Wagner Filho <wagfilho@hotmail.com.br>
2021-06-09 22:02:02 -0300

    Second version

--- app/src/main/java/com/devventure/rollingdices/MainActivity.kt
+++ app/src/main/java/com/devventure/rollingdices/MainActivity.kt
@@ -2,6 +2,7 @@ package com.devventure.rollingdices
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -26,9 +27,32 @@ class MainActivity : AppCompatActivity() {
         btn.setOnClickListener{
             dice01.setImageResource(images.random())
             dice02.setImageResource(images.random())
+
+            Log.i("CICLO DE VIDA", "ON CREATE")
         }
     }
+    // Métodos para monitorar ciclo de vida
+    /*
+    override fun onStart() {
+        super.onStart()
+        Log.i("CICLO DE VIDA", "ON START")
+    }
 
+    override fun onResume() {
+        super.onResume()
+        Log.i("CICLO DE VIDA", "ON RESUME")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("CICLO DE VIDA", "ON STOP")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("CICLO DE VIDA", "ON DESTROY")
+    }
+    */
     private fun randomNumber() : Int {
         return (1..6).random()
     }
