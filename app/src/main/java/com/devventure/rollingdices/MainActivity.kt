9c7a80ae3279c22741eb396136c709cf5e83d0cf
Wagner Filho <wagfilho@hotmail.com.br>
2021-06-09 21:18:18 -0300

    Second version

--- app/src/main/java/com/devventure/rollingdices/MainActivity.kt
+++ app/src/main/java/com/devventure/rollingdices/MainActivity.kt
@@ -3,6 +3,7 @@ package com.devventure.rollingdices
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dice01 = findViewById<TextView>(R.id.dice01)
-        val dice02 = findViewById<TextView>(R.id.dice02)
+        val dice01 = findViewById<ImageView>(R.id.dice01)
+        val dice02 = findViewById<ImageView>(R.id.dice02)
         val btn = findViewById<Button>(R.id.button)
 
+        val images = listOf(
+            R.drawable.dice_1,
+            R.drawable.dice_2,
+            R.drawable.dice_3,
+            R.drawable.dice_4,
+            R.drawable.dice_5,
+            R.drawable.dice_6)
+
         btn.setOnClickListener{
-            dice01.text = randomNumber().toString()
-            dice02.text = randomNumber().toString()
+            dice01.setImageResource(images.random())
+            dice02.setImageResource(images.random())
         }
     }
 
